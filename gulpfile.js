6343ee53b1f547eebd7f1c4ca4f2bdab20e32124
jessabean <jessica.avison@gmail.com>
2018-04-10 16:55:23 -0400

    Update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass        = require('gulp-sass');
 gulp.task('browser-sync', ['sass'], function() {
   browserSync({
     server: {
-      baseDir: './'
+      baseDir: './src'
     }
   });
 });
@@ -16,14 +16,14 @@ gulp.task('browser-reload', function() {
 });
 
 gulp.task('sass', function () {
-    return gulp.src('sass/main.scss')
+    return gulp.src('src/sass/main.scss')
         .pipe(sass({
             includePaths: ['sass'],
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('express', function() {
@@ -33,10 +33,25 @@ gulp.task('express', function() {
   app.listen(4000, '0.0.0.0');
 });
 
-
 gulp.task('watch', function () {
-    gulp.watch('sass/**/*.scss', ['sass']);
-    gulp.watch('**/*.html', ['browser-reload']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/**/*.html', ['browser-reload']);
 });
 
+gulp.task('html', function() {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/css/main.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/app.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build', ['html', 'css', 'js']);
 gulp.task('default', ['express', 'browser-sync', 'watch']);
