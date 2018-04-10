3abe47e94a6dc666491fabdadd451fb529715c77
jessabean <jessica.avison@gmail.com>
2018-04-10 16:05:07 -0400

    Set up project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Prototyping Layout with CSS Grid
+
+Source code for prototype presented at Revolution Conf May 2018.
+
+## Quick start
+
+```
+git clone git@github.com:jessabean/grid-prototype-refresh-dc.git
+cd grid-prototype-refresh-dc
+npm install
+node server.js
+```
+
+To make changes to the code and compile `.scss` files, run `gulp watch`.
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var prefix      = require('gulp-autoprefixer');
+var sass        = require('gulp-sass');
+
+gulp.task('browser-sync', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('browser-reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('sass', function () {
+    return gulp.src('sass/main.scss')
+        .pipe(sass({
+            includePaths: ['sass'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname));
+  app.listen(4000, '0.0.0.0');
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('**/*.html', ['browser-reload']);
+});
+
+gulp.task('default', ['express', 'browser-sync', 'watch']);
