6a3cec720cfc4174d14cc06f20d37d1e0b9a28fc
jessabean <jess@eldredge.io>
2018-05-14 10:03:17 -0400

    Add gulp build step

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,62 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var prefix      = require('gulp-autoprefixer');
-var sass        = require('gulp-sass');
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var prefix = require("gulp-autoprefixer");
+var sass = require("gulp-sass");
 
-gulp.task('browser-sync', ['sass'], function() {
+var prefixOptions = {
+  browsers: ["last 2 versions", "ie >= 11", "ios >= 7", "android >= 4.4.2"],
+  grid: false
+};
+
+gulp.task("browser-sync", ["sass"], function() {
   browserSync({
     server: {
-      baseDir: './src'
+      baseDir: "./src"
     }
   });
 });
 
-gulp.task('browser-reload', function() {
+gulp.task("browser-reload", function() {
   browserSync.reload();
 });
 
-gulp.task('sass', function () {
-    return gulp.src('src/sass/main.scss')
-        .pipe(sass({
-            includePaths: ['sass'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('src/css'));
+gulp.task("sass", function() {
+  return gulp
+    .src("src/sass/main.scss")
+    .pipe(
+      sass({
+        includePaths: ["sass"],
+        onError: browserSync.notify
+      })
+    )
+    .pipe(prefix(prefixOptions))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest("src/css"));
 });
 
-gulp.task('express', function() {
-  var express = require('express');
+gulp.task("express", function() {
+  var express = require("express");
   var app = express();
   app.use(express.static(__dirname));
-  app.listen(4000, '0.0.0.0');
+  app.listen(4000, "0.0.0.0");
 });
 
-gulp.task('watch', function () {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
-    gulp.watch('src/**/*.html', ['browser-reload']);
+gulp.task("watch", function() {
+  gulp.watch("src/sass/**/*.scss", ["sass"]);
+  gulp.watch("src/**/*.html", ["browser-reload"]);
 });
 
-gulp.task('html', function() {
-  return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+gulp.task("html", function() {
+  return gulp.src("src/index.html").pipe(gulp.dest("dist"));
 });
 
-gulp.task('css', function() {
-  return gulp.src('src/css/main.css')
-    .pipe(gulp.dest('dist/css'));
+gulp.task("css", function() {
+  return gulp.src("src/css/main.css").pipe(gulp.dest("dist/css"));
 });
 
-gulp.task('js', function() {
-  return gulp.src('src/js/app.js')
-    .pipe(gulp.dest('dist/js'));
+gulp.task("js", function() {
+  return gulp.src("src/js/app.js").pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('build', ['html', 'css', 'js']);
-gulp.task('default', ['express', 'browser-sync', 'watch']);
+gulp.task("build", ["html", "css", "js"]);
+gulp.task("default", ["express", "browser-sync", "watch"]);
